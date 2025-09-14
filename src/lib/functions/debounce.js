25414f51daf1e9f5ed684780103ea5d0e4799a99
thisnabeel <nabeel.khan.swe@gmail.com>
2025-09-13 17:50:17 -0700

    juz

--- src/lib/functions/debounce.js
+++ src/lib/functions/debounce.js
@@ -1,14 +1,13 @@
-
-import Api from "$lib/api/api";
+import Api from '$lib/api/api';
 
 let updateTimer;
 function save(url, payload, action = null) {
-    clearTimeout(updateTimer);
-    updateTimer = setTimeout(async () => {
-        const response = await Api.put(url+ '.json', payload);
-        // let response = await Api.get("/abstractions/"+abstraction.id+".json")
-        console.log('response', response);
-    }, 1000);
+	clearTimeout(updateTimer);
+	updateTimer = setTimeout(async () => {
+		const response = await Api.put(url + '.json', payload);
+		// let response = await Api.get("/abstractions/"+abstraction.id+".json")
+		console.log('response', response);
+	}, 1000);
 }
 
 export default save;
--- src/lib/functions/params.js
+++ src/lib/functions/params.js
@@ -1,5 +1,5 @@
 function getQueryParam(name) {
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    return urlSearchParams.get(name);
+	const urlSearchParams = new URLSearchParams(window.location.search);
+	return urlSearchParams.get(name);
 }
 export default getQueryParam;
